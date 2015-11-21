683144b0c3b97fb5e799b67365ce25fa8461909f
OmarAlobaid <alobaid@gwu.edu>
2015-11-20 22:33:34 -0500

    test

---
+++ ParseStarterProject/src/main/java/com/parse/starter/Constants.java
@@ -0,0 +1,21 @@
+package com.parse.starter;
+
+
+public class Constants {
+    public static final String PARSE_APP_ID = "E6nml5ahUWOBUMy56TFFOQvqjtcCAhsSGRr0L4gD";
+    public static final String PARSE_CLIENT_ID = "7GMmQNtklSapF6cJO1sxkDPjTCdGj5X62Z2nAmR9";
+    public static final String TO_NAME = "toName";
+    public static final String FROM_NAME = "fromName";
+    public static final int FOLLOWING = 5;
+    public static final int FOLLOWERS = 4;
+    public static final int TIMELINE = 0;
+    public static final int GOAL_DEATAL = 3;
+    public static final int SETTING = 2;
+    public static final int BROWSE = 1;
+    public static final int CAMERA = 10;
+    public static final int MiNIMUM_STEPS = 5;
+
+
+
+
+}
